8b869f597b129f724b417f49506dbdd290566101
RocketCoindexter <RocketCoindexter@rocketmail.com>
2012-06-11 21:25:33 -0500

    Use Twitter Bootstrap

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KickBank::Application.config.secret_token = '0e44c44f33f59c19c09607c873fccbf334b5edfcdcc73ac438ea3dbd05cbc96b4fc647736dd38349d59d314778b527103d08abf3398ce1c4334c68af8760c56b'
